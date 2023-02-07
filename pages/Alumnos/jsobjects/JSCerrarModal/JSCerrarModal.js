25f87ad54507916fa7b903fd34916d0cb27e611d
JG <jgalan@iesguadalquivir.net>
2023-02-07 22:25:37 +0000

    a

---
+++ pages/Alumnos/jsobjects/JSCerrarModal/JSCerrarModal.js
@@ -0,0 +1,12 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	CerrarModal: () => {
+		//write code here
+		{{closeModal(Modal1)}};
+		{{showModal(ModalOk)}};
+	},
+	myFun2: async () => {
+		//use async-await or promises
+	}
+}
